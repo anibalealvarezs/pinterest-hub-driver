c73c67f737b52351eb785d9f6873415d9d08f03d
Anibal Alvarez <aalvarez@chmw.co>
2026-04-23 15:20:40 -0400

    Fix: Resolve trait collisions by removing redundant legacy trait

--- src/Drivers/PinterestDriver.php
+++ src/Drivers/PinterestDriver.php
@@ -89,7 +89,6 @@ class PinterestDriver implements SyncDriverInterface
     {
         return null;
     }
-    use HasUpdatableCredentials;
 
     private ?AuthProviderInterface $authProvider = null;
     private ?LoggerInterface $logger = null;
